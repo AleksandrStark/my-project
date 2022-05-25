ff3e4a1c6b0efb596589494153adcd5745985104
Aleksandr Starkov <77936849+AleksandrStark@users.noreply.github.com>
2022-05-25 19:02:00 +0300

    follow - unfollow post, delete requests

--- src/components/users/UsersPres.js
+++ src/components/users/UsersPres.js
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './Users.module.css';
 import userPhoto from '../../images/user.png';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 let UsersPres = (props) => {
 	let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
@@ -44,7 +45,21 @@ let UsersPres = (props) => {
 							{u.followed ? (
 								<button
 									onClick={() => {
-										props.unfollow(u.id);
+										axios
+											.delete(
+												`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
+												{
+													withCredentials: true,
+													headers: {
+														'API-KEY': '4ac2f20d-f55d-4df8-b058-f695982a7ceb',
+													},
+												}
+											)
+											.then((response) => {
+												if (response.data.resultCode === 0) {
+													props.unfollow(u.id);
+												}
+											});
 									}}
 								>
 									Unfollow
@@ -52,7 +67,22 @@ let UsersPres = (props) => {
 							) : (
 								<button
 									onClick={() => {
-										props.follow(u.id);
+										axios
+											.post(
+												`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
+												{},
+												{
+													withCredentials: true,
+													headers: {
+														'API-KEY': '4ac2f20d-f55d-4df8-b058-f695982a7ceb',
+													},
+												}
+											)
+											.then((response) => {
+												if (response.data.resultCode === 0) {
+													props.follow(u.id);
+												}
+											});
 									}}
 								>
 									Follow
