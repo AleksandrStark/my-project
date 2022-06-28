d6efa00f0e411bf4628d1fdc392d4d2f45f104fa
Aleksandr Starkov <77936849+AleksandrStark@users.noreply.github.com>
2022-06-28 20:43:50 +0300

    redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import authReducer from './authReducer';
 import dialogReducer from './dialogReducer';
 import profileReducer from './profileReducer';
@@ -18,8 +18,11 @@ let reducers = combineReducers({
 	app: appReducer,
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleWare));
-
-window.store = store;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+	reducers,
+	composeEnhancers(applyMiddleware(thunkMiddleWare))
+);
+// window.__store__ = store;
 
 export default store;
